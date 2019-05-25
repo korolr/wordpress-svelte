8851854a8ce8d2f1d72bb420293a3ab921170141
Alexsey <ekstazu@gmail.com>
2019-05-25 15:52:32 +0400

    wordpress support

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * Enqueue scripts and styles.
+ */
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>                                                                                                                             
+<html <?php language_attributes(); ?>>                                                                                                      
+        <head>                                                                                                                              
+                <meta charset="<?php bloginfo( 'charset' ); ?>">                                                                            
+                <meta name="viewport" content="width=device-width,initial-scale=1">                                                         
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/public/favicon.png">                                    
+        <link rel='stylesheet' href='<?php echo get_template_directory_uri(); ?>/public/global.css'>                                        
+            <link rel='stylesheet' href='<?php echo get_template_directory_uri(); ?>/public/bundle.css'>                                    
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >    
+                <title><?php bloginfo('name')?></title>                                                                                     
+                <?php wp_head(); ?>                                                                                                         
+        </head>                                                                                                                             
+        <body>                                                                                                                              
+                                                                                                                                            
+                                                                                                                                            
+                                                                                                                                            
+        <?php wp_footer(); ?>                                                                                                               
+                                                                                                                                            
+                <script src='<?php echo get_template_directory_uri(); ?>/public/bundle.js'></script>                                        
+        </body>                                                                                                                             
+</html>                                                                                                                                     
